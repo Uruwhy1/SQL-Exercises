826606b4b5afe6534699b9877be7a750f22cc0bd
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2024-07-26 13:03:53 -0300

    finish 07 exercises bundle

--- 07-more-join-operations.sql
+++ 07-more-join-operations.sql
@@ -83,3 +83,81 @@ SELECT DISTINCT title
     WHERE name = 'Harrison Ford'
     AND casting.ord != 1
 )
+
+/*
+10. List the films together with the leading star for all 1962 films. 
+*/
+
+SELECT DISTINCT title
+  FROM movie JOIN casting ON movieid = movie.id
+  JOIN actor ON actor.id = actorid
+  WHERE actorid = (
+    SELECT id FROM actor
+    WHERE casting.ord = 1
+)
+  WHERE yr = 1962
+
+/*
+11. Which were the busiest years for 'Rock Hudson', show the year and the number of movies he made each year for any year in which he made more than 2 movies. 
+*/
+
+SELECT yr,COUNT(title) FROM
+  movie JOIN casting ON movie.id=movieid
+        JOIN actor   ON actorid=actor.id
+WHERE name='Rock Hudson'
+GROUP BY yr
+HAVING COUNT(title) > 2
+
+/*
+12. List the film title and the leading actor for all of the films 'Julie Andrews' played in. 
+*/
+
+SELECT title, name
+  FROM movie JOIN casting ON (movieid = movie.id
+                              AND ord=1)
+             JOIN actor ON (actorid = actor.id)
+WHERE movie.id IN (
+  SELECT movieid FROM casting
+    WHERE actorid IN (
+      SELECT id FROM actor
+        WHERE name = 'Julie Andrews'
+    )   
+)
+
+/*
+13. Obtain a list, in alphabetical order, of actors who've had at least 15 starring roles. 
+*/
+
+SELECT name
+  FROM casting
+  JOIN actor ON (id = actorid AND ord=1)
+  GROUP BY actorid
+  HAVING COUNT(actorid) >= 15
+  ORDER BY name
+
+/*
+14.  List the films released in the year 1978 ordered by the number of actors in the cast, then by title. 
+*/
+
+SELECT DISTINCT title, COUNT(actorid) 
+  FROM movie JOIN casting ON (movie.id = movieid)
+  WHERE yr = 1978
+  GROUP BY movieid
+  ORDER BY COUNT(actorid) DESC, title
+
+/*
+15. List all the people who have worked with 'Art Garfunkel'. 
+*/
+
+SELECT name
+  FROM movie JOIN casting ON (movieid = movie.id
+                             )
+             JOIN actor ON (actorid = actor.id)
+WHERE movie.id IN (
+  SELECT movieid FROM casting
+    WHERE actorid IN (
+      SELECT id FROM actor
+        WHERE name = 'Art Garfunkel'
+    )   
+)
+AND name != 'Art Garfunkel'
